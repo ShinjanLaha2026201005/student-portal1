a84f2b7a44d38510fc4798057201d37bc2192db5
ShinjanLaha2026201005 <shinjan.laha@students.iiit.ac.in>
2026-08-15 23:13:43 +0530

    Revert "feat(settings): add accessibility text scaling option"
    This reverts commit ab10aac94cd931dbd0ad20980a2b85a09fffe853.

--- src/settings.cpp
+++ src/settings.cpp
@@ -3,17 +3,12 @@ struct Settings {
     bool darkMode = false;
     bool notificationsOn = true;
 };
-
 void toggleDarkMode(Settings& s) {
     s.darkMode = !s.darkMode;
 }
-
 void toggleNotifications(Settings& s) {
     s.notificationsOn = !s.notificationsOn;
 }
-
 void printSettings(const Settings& s) {
     std::cout << "Dark mode: " << s.darkMode << ", Notifications: " << s.notificationsOn << "\n";
 }
-// accidental settings edit made on main
-// accidental settings edit made on main
